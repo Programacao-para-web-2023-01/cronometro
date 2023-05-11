53c30edbc9f9543c11d63b0bed676281a33ef005
gadelima <gabriel.delima@mercadolibre.com>
2023-05-11 16:31:20 -0300

    feat: cronometros implementados

--- app/page.tsx
+++ app/page.tsx
@@ -4,27 +4,14 @@ import {useEffect, useState} from "react";
 import Contador from "@/app/components/Contador";
 import Reseter from "@/app/components/Reseter";
 import {clearInterval} from "timers";
+import Cronometro from "@/app/components/Cronometro";
+import Cronometro2 from "@/app/components/Cronometro2";
 
 export default function Home() {
-    const [contador, setContador] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(
-            () => setContador((atual) => atual+1),
-            1000)
-
-        return () => {
-            clearInterval(interval)
-        }
-    }, [])
-
     return (
-      <main className="flex min-h-screen flex-col items-center p-24">
-          <Contador
-              contador={contador}
-              onIncrement={() => setContador(contador+1)}
-          />
-          <Reseter onReset={() => setContador(0)} />
+      <main className="flex min-h-screen flex-col items-center p-24 gap-4">
+          <Cronometro/>
+          <Cronometro2/>
       </main>
     )
 }
